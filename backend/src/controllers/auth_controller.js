e187e37c3c8b410523ed0e519565a06bdae73138
belenaguilarv <belenaguiilarv@gmail.com>
2024-01-30 19:13:36 -0300

    se crearon token y cookies

--- backend/src/controllers/auth_controller.js
+++ backend/src/controllers/auth_controller.js
@@ -1,20 +1,33 @@
 import User from "../models/user_model.js"
+import bcrypt from 'bcryptjs'
+import {createAccessToken} from "../libs/jwt.js"
 
 export const login = (req, res) => res.send("login");
 
 export const register = async(req, res) => {
-    const {username, email, password} = req.body
+    const {username, email, password} = req.body;
 
     try{
+        const passwordhash = await bcrypt.hash(password, 10);
+
         const newUser = new User({
             username,
             email,
-            password
-        })
+            password: passwordhash,
+        });
+
         const userSaved = await newUser.save();
-        res.json(userSaved)
+
+        const token = await createAccessToken({id: userSaved._id});
+
+        res.cookie('token',token)
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+        });
     }catch(error){
-        console.log(error);
-    }
-}
+            res.status(500).json({message: error.message});
+        }
+};
 
