6eee6afd80a2cfece9ea6c9c32084cc2520742d6
belenaguilarv <belenaguiilarv@gmail.com>
2024-01-31 09:51:54 -0300

    Funcion de login creada

--- backend/src/controllers/auth_controller.js
+++ backend/src/controllers/auth_controller.js
@@ -2,7 +2,29 @@ import User from "../models/user_model.js"
 import bcrypt from 'bcryptjs'
 import {createAccessToken} from "../libs/jwt.js"
 
-export const login = (req, res) => res.send("login");
+export const login = async(req, res) => {
+    const {username, password} = req.body;
+
+    try{
+
+        const userFound = await User.findOne({username});
+        if(!userFound) return res.status(400).json({message:"User not found"});
+
+        const isMatch = await bcrypt.compare(password, userFound.password);
+        if(!isMatch) return res.status(400).json({message:"Invalid credential"});
+
+        const token = await createAccessToken({id: userFound._id});
+
+        res.cookie('token',token)
+        res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+        });
+    }catch(error){
+            res.status(500).json({message: error.message});
+        }
+};
 
 export const register = async(req, res) => {
     const {username, email, password} = req.body;
