bd85f4bd457792ee3b5521ea0dfb5b99544ae8dd
belenaguilarv <belenaguiilarv@gmail.com>
2024-02-06 12:52:18 -0300

    Se agregó CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
 
 import authRoutes from './routes/auth_routes.js';
 import taskRoutes from './routes/tasks_routes.js';
@@ -8,6 +10,9 @@ import taskRoutes from './routes/tasks_routes.js';
 
 const app = express();
 
+app.use(cors({
+    origin:'http://localhost:5173',
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
